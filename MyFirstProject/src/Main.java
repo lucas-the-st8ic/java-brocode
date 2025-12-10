1b4dd17519d8c042a4db65629030b21edfdcc84c
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-10 18:02:28 -0300

    Initial commit

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void main(String[] args) {
+
+        //THYS IS MY FIRST JAVA PROGRAM
+        /*
+            This
+            is
+            a
+            multi-line
+            comment
+        */
+        System.out.println("I like Pizza!!!");
+        System.out.println("It's really good!!!");
+        System.out.println("Buy me pizza!!");
+        System.out.println();
+    }
+}
