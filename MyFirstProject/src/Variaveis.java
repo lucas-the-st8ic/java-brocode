0f101aa61a3f75ae5418369310949be1571b232d
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-13 10:23:19 -0300

    Create Variaveis.java

---
+++ MyFirstProject/src/Variaveis.java
@@ -0,0 +1,27 @@
+public class Variaveis {
+    static void main(String[] args) {
+
+        int age = 30;
+        int year = 1998;
+        int qt = 1;
+
+        double price = 19.99;
+        double avaliacao = 3.5;
+        double temperature = -12.5;
+
+        char grade = 'A';
+        char symbol = '@';
+        char currency = '$';
+
+        boolean isStudent = true;
+        boolean forSale = false;
+
+        String name = "Lucas";
+        String favFood = "Pizza";
+        String eMail = "fake123@gmail.com";
+        String car = "Pontiac GTO";
+
+        System.out.println(name+ " your favorite food is: " +favFood+"!!");
+
+    }
+}
