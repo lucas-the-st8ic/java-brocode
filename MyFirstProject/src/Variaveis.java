7a5cae141732c62824042b798fd513cd7919b8f5
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-15 10:03:26 -0300

    Update Variaveis.java

--- MyFirstProject/src/Variaveis.java
+++ MyFirstProject/src/Variaveis.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Variaveis {
     static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        int age = 30;
+
+        int age;
         int year = 1998;
         int qt = 1;
 
@@ -13,15 +17,40 @@ public class Variaveis {
         char symbol = '@';
         char currency = '$';
 
-        boolean isStudent = true;
+        boolean isStudent;
         boolean forSale = false;
 
-        String name = "Lucas";
+        String name;
         String favFood = "Pizza";
         String eMail = "fake123@gmail.com";
         String car = "Pontiac GTO";
 
-        System.out.println(name+ " your favorite food is: " +favFood+"!!");
+        System.out.println("\nYour favorite food is: " +favFood+"!!");
 
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        age = input.nextInt();
+
+        System.out.print("Enter your note: ");
+        double note = input.nextDouble();
+
+        System.out.println("Are you a student? (true/false): ");
+        isStudent = input.nextBoolean();
+
+        System.out.println("Hello " + name);
+        System.out.println("Your age is " + age);
+        System.out.println("Your note is " + note);
+
+        if(isStudent){
+            System.out.println("You're enrolled as a student");
+        } else {
+            System.out.println("You're not enrolled as a student");
+        }
+
+
+        input.close();
     }
 }
