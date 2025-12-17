48d97663a3560b502b0b497f674ce27afe3137b1
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-17 16:56:34 -0300

    Update MadLibs.java

--- MyFirstProject/src/MadLibs.java
+++ MyFirstProject/src/MadLibs.java
@@ -12,10 +12,24 @@ public class MadLibs {
         String verb1;
         String adjective3;
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.print("Enter and adjective(description): ");
+        adjective1 = input.nextLine();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = input.nextLine();
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = input.nextLine();
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = input.nextLine();
+        System.out.print("Enter and adjective(description): ");
+        adjective3 = input.nextLine();
 
+
+        System.out.println("\nToday I went to a " +adjective1+ " zoo.");
+        System.out.println("In an exhibit, I saw a " +noun1+ ".");
+        System.out.println(noun1+ " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "! ");
+
+
+        input.close();
     }
 }
