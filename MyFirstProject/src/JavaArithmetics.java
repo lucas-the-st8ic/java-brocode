e8a006eb51c723a63b459aa879cf781cbb964c17
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-19 17:03:55 -0300

    Create JavaArithmetics.java

---
+++ MyFirstProject/src/JavaArithmetics.java
@@ -0,0 +1,28 @@
+public class JavaArithmetics {
+    static void main(String[] args) {
+
+        //Arithmetics Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        // z = x % y;
+
+
+        //AUGMENTED ASSIGNMENT OPERATORS
+
+        //x += Y;
+        //x -= Y;
+        //x *= Y;
+        //x /= y;(cuidado com os tipos inteiros e reais para não perder os decimais.)
+
+
+
+        System.out.println(z);
+    }
+}
