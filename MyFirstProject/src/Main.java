7a2d7c70bbb8c66665cbd796987ce92aa18b6b3d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-11 14:47:34 -0300

    updates

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -2,19 +2,6 @@
 import java.util.Random;
 
 static void main(String[] args) {
-
-    Random random = new Random();
-
-    int number;
-
-    number = random.nextInt(1, 7);
-
-    System.out.println("Random Number: " +number);
-
-    double number2;
-
-    number2 = random.nextDouble(0, 10.90);
-
-    System.out.printf("Random Number: %.2f", number2);
+/*Java Bro Code Playlist*/
 }
 
---
+++ MyFirstProject/src/MathClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MathClass {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Número de PI com a classe Math(Math.PI): "+Math.PI);
+        System.out.println("Número Exponencial com a classe Math(Math.E): "+Math.E);
+
+        /*double result;
+
+        result = Math.pow(2, 5);
+        result = Math.abs(-5);
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.99);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
+        System.out.println(result);*/
+
+        //HYPOTENUSE c = Math.sqrt(a² + b²)
+        
+    }
+}
