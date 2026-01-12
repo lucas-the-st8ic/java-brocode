7852276455a56642d730df8cdd1dd8d8253673b1
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-12 13:30:33 -0300

    Update MathClass.java

--- MyFirstProject/src/MathClass.java
+++ MyFirstProject/src/MathClass.java
@@ -35,6 +35,26 @@ public class MathClass {
 
         System.out.println("The hypotenuse (side c) is: " + c + "cm.");
 
+        /*circumference = 2 * Math.PI * radius;
+        * area = Math.PI * Math.pow(radius, 2);
+        * volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);*/
+
+        double radius;
+        double circumference;
+        double area;
+        double volume;
+
+        System.out.print("Enter the radius: ");
+        radius = input.nextDouble();
+
+        circumference = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+        volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+        System.out.printf("The circumference is:  %.1fcm", circumference);
+        System.out.printf("\nThe area is: %.1fcm²", area);
+        System.out.printf("\nThe volume is: %.1fcm³", volume);
+
         input.close();
     }
 }
