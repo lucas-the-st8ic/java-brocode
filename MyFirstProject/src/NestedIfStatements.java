6671573fda0b02a3da05371c02c80735698b7b13
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-15 17:11:15 -0300

    Update NestedIfStatements.java

--- MyFirstProject/src/NestedIfStatements.java
+++ MyFirstProject/src/NestedIfStatements.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class NestedIfStatements {
     static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+
+
+        if (isStudent) {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            } else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+
+        } else {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.8;
+            }
+        }
+
+        System.out.printf("The price of a ticket is: R$%.2f", price);
     }
 }
