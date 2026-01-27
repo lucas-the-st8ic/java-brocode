638b6d61f4a86db6cd4585218eae1bf4015edb39
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-27 17:19:32 -0300

    Create WeightConverter.java

---
+++ MyFirstProject/src/WeightConverter.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class WeightConverter {
+    static void main(String[] args) {
+
+        // Weight Conversion Program
+
+        Scanner input = new Scanner(System.in);
+
+        //Declare variabels
+
+        double weight;
+        double newWeight;
+        int choice;
+
+        //Welcome message
+
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert lbs to Kgs");
+        System.out.println("2: Convert Kgs to lbs");
+
+        
+        //Prompt for user choice
+
+        System.out.print("Choose an option: ");
+        choice = input.nextInt();
+
+        //Option 1 convert lbs to Kgs
+
+        //Option 2 convert Kgs to lbs
+
+        // Else print not a valid choice
+    }
+}
